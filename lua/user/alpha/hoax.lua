47962da1384db1c58258348b85380ed056af5f7e
H0ax0 <josuedenis328@gmail.com>
2022-07-19 16:25:38 -0400

    fix project openning

--- lua/user/alpha/hoax.lua
+++ lua/user/alpha/hoax.lua
@@ -12,8 +12,9 @@ end
 local dashboard = require("alpha.themes.dashboard")
 
 local U = require("user.utils.utils")
-local function file_button(fn, sc, short_fn)
+local function file_button(fn, sc, short_fn, keybind_opts)
 	local btn_text, btn_hl = U.render_startship_text(fn, short_fn, sc)
+	local sc_ = sc:gsub("%s", "")
 
 	local opts = {
 		position = "center",
@@ -25,9 +26,11 @@ local function file_button(fn, sc, short_fn)
 		hl_shortcut = btn_hl,
 		shrink_margin = false,
 	}
+        keybind_opts = vim.F.if_nil(keybind_opts, { noremap = true, silent = true, nowait = true })
+        opts.keymap = { "n", sc_, ":cd " .. fn .."<CR> :NvimTreeOpen <CR>", keybind_opts }
 
 	local function on_press()
-		local key = vim.api.nvim_replace_termcodes(sc .. "<Ignore>", true, false, true)
+		local key = vim.api.nvim_replace_termcodes(sc_ .. "<Ignore>", true, false, true)
 		vim.api.nvim_feedkeys(key, "t", false)
 	end
 
